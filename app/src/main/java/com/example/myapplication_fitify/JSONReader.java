6616e8ee3a310c5611775b44dcbd94c26e06c330
ioana-dumitrache <yoanna_dumitrache@yahoo.com>
2021-11-15 21:58:57 +0200

    Tema04

---
+++ app/src/main/java/com/example/myapplication_fitify/JSONReader.java
@@ -0,0 +1,70 @@
+package com.example.myapplication_fitify;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONReader {
+    public void read(String urlpath, IResponse response){
+        try {
+            URL url=new URL(urlpath);
+            HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+            InputStream input=connection.getInputStream();
+            InputStreamReader inputReader=new InputStreamReader(input);
+            BufferedReader reader=new BufferedReader(inputReader);
+            StringBuilder result=new StringBuilder();
+            String line="";
+            while((line=reader.readLine())!=null){
+                result.append(line);
+
+            }
+            reader.close();
+            inputReader.close();
+            input.close();
+
+            List<Exercitii> list=parsare(result.toString());
+            response.onSuccess(list);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            response.onError(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.onError(e.getMessage());
+        }
+
+    }
+    private List<Exercitii> parsare(String jsonString)
+    {
+        List<Exercitii> lista=new ArrayList<>();
+        try {
+            JSONObject jsonObject=new JSONObject(jsonString);
+            JSONArray jsonArray=jsonObject.getJSONArray("exercitii");
+            for(int i=0;i<jsonArray.length();i++)
+            {
+                JSONObject currentObject=jsonArray.getJSONObject(i);
+                String denumire=currentObject.getString("denumire");
+                int durata=currentObject.getInt("durata");
+                String descriere=currentObject.getString("descriere");
+                Exercitii exercitii=new Exercitii(denumire, durata, descriere);
+                lista.add(exercitii);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return lista;
+
+    }
+
+}
--- app/src/main/java/com/example/myapplication_fitify/MainActivity3.java
+++ app/src/main/java/com/example/myapplication_fitify/MainActivity3.java
@@ -13,39 +13,71 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity3 extends AppCompatActivity {
-private ArrayList<String> items;
-private ArrayAdapter<String> itemsAdapter;
-private ListView listView;;
-private Button button;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
+    private Adaptor adaptor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        listView=findViewById(R.id.listView);
-        button=findViewById(R.id.buttonListView);
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.buttonListView);
 
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 addItem(view);
             }
         });
-        items=new ArrayList<>();
-        itemsAdapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-      listView.setAdapter(itemsAdapter);
-      setUpListViewListener();
-    }
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpAdapter();
 
-    private void setUpListViewListener() {
+        JSONReader reader = new JSONReader();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                reader.read("https://jsonkeeper.com/b/YZRO", new IResponse() {
+
+                    @Override
+                    public void onSuccess(List<Exercitii> exercitii) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                adaptor.updateList(exercitii);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(String mesaj) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity3.this, mesaj, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
+        thread.start();
+
+    }
+    private void setUpAdapter() {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long id) {
-                Context context=getApplicationContext();
+                Context context = getApplicationContext();
                 Toast.makeText(context, "Sters ", Toast.LENGTH_LONG).show();
                 items.remove(String.valueOf(i));
                 itemsAdapter.notifyDataSetChanged();
@@ -55,17 +87,24 @@ private Button button;
     }
 
     private void addItem(View v) {
-        EditText input=findViewById(R.id.editTextListView);
-        String itemText=input.getText().toString();
+        EditText input = findViewById(R.id.editTextListView);
+        String itemText = input.getText().toString();
 
-        if(!(itemText.equals("")))
-        {
+        if (!(itemText.equals(""))) {
             itemsAdapter.add(itemText);
             input.setText("");
-        }
-        else
-        {
+        } else {
             Toast.makeText(getApplicationContext(), "Completeaza", Toast.LENGTH_LONG).show();
         }
+    }
+    private List<Exercitii> getLista() {
+        ArrayList<Exercitii> lista = new ArrayList<>();
+        Exercitii ex1 = new Exercitii("Triceps", 10f, "Se folosesc ganterele.");
+        Exercitii ex2 = new Exercitii("Piept", 15f, "Se foloseste bara olimpica. ");
+
+        lista.add(ex1);
+        lista.add(ex2);
+
+        return lista;
     }
 }
